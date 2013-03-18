4c46b3cef15da91d427e972a74237c8147e774c1
Matthew Fabb <Matt_Fabb@yahoo.com>
2013-03-17 20:02:15 -0400

    Checking for matching strings
    Checks for a particular string and then gives an alert if one is found
    in final transcript.

--- js/web_speech_script.js
+++ js/web_speech_script.js
@@ -101,6 +101,15 @@ if (!('webkitSpeechRecognition' in window)) {
             }
         }
 
+        if (interim_transcript.indexOf(match_string) > -1) {
+            match_results.innerHTML = "Possible match found for: " + match_string;
+        }
+
+        if (final_transcript.indexOf(match_string) > -1) {
+            alert(match_string);
+            match_results.innerHTML = "Match found for: " + match_string;
+        }
+
         final_transcript = capitalize(final_transcript);
         final_span.innerHTML = linebreak(final_transcript);
         interim_span.innerHTML = linebreak(interim_transcript);
@@ -151,3 +160,7 @@ function showInfo(s) {
         info.style.visibility = 'hidden';
     }
 }
+
+var match_string = "next";
+match_results = document.getElementById('match_results');
+match_results.innerHTML = "No match found for '" + match_string + "'";
